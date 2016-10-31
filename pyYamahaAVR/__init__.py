0cdc0cd46d3061c8460f0f5176292b112621e5e4
Jens Schmer <jens.schmer+git@gmail.com>
2016-10-31 09:54:28 +0100

    register NullHandler() for logging

--- pyYamahaAVR/__init__.py
+++ pyYamahaAVR/__init__.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from .exception import *
+from .util import get_module_logger
 from .avr import YamahaAPI
+
+# Register a 'do-nothing' handler to avoid the
+# 'No handlers could be found for logger "xyz"' message
+# in case of log messages >= WARNING and the client didn't configure logging
+get_module_logger().addHandler(logging.NullHandler())
 
 __all__ = [
     'AVRException',
